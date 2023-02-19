f464af3430fc56c3442b33e74413e0e9a15cd3be
Lee <1769712655@qq.com>
2023-02-19 19:43:43 +0800

    feat: better benchmark

--- benches/gcbench-plimmix.rs
+++ benches/gcbench-plimmix.rs
@@ -1,11 +1,12 @@
 use std::{mem::size_of, thread::available_parallelism, time::Duration};
 
-use criterion::{criterion_group, criterion_main, Criterion};
+use bdwgcvsimmix_bench::{GC_unregister_my_thread, Heap};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use immix::*;
 
 fn immix_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("plimmixgc");
-    group.sample_size(10);
+    let mut group = c.benchmark_group("gc");
+    group.sample_size(30);
     gc_enable_auto_collect();
     // 这个测试中并没有记录所有的gcroot，如果启用evacuation
     // 可能会导致部分指针驱逐后不自愈
@@ -30,15 +31,31 @@ fn immix_benchmark(c: &mut Criterion) {
         //     },
         // );
         no_gc_thread();
-        group.bench_function(
-            format!(
-                "multi-thread ({}) gc stress benchmark small objects",
-                get_threads()
-            ),
-            |b| {
+        let mut heap = Heap::new();
+        for t in 0..get_threads() {
+            let t = t + 1;
+            group.bench_with_input(BenchmarkId::new("bdw", t), &t, |b, i| {
+                b.iter(|| {
+                    let mut threads = Vec::with_capacity(*i);
+                    for _ in 0..*i {
+                        threads.push(std::thread::spawn(move || {
+                            heap.register_current_thread();
+                            bdwgcbench(&mut heap);
+                            unsafe {
+                                GC_unregister_my_thread();
+                            }
+                        }));
+                    }
+
+                    while let Some(th) = threads.pop() {
+                        th.join().unwrap();
+                    }
+                });
+            });
+            group.bench_with_input(BenchmarkId::new("immix", t), &t, |b, i| {
                 b.iter(|| {
                     let mut threads = Vec::with_capacity(4);
-                    for _ in 0..get_threads() {
+                    for _ in 0..*i {
                         threads.push(std::thread::spawn(move || {
                             SPACE.with(|space| {
                                 let mut space = space.borrow_mut();
@@ -52,8 +69,8 @@ fn immix_benchmark(c: &mut Criterion) {
                         th.join().unwrap();
                     }
                 });
-            },
-        );
+            });
+        }
     }
 }
 
@@ -173,6 +190,114 @@ const K_MAX_TREE_DEPTH: i32 = 16;
 
 fn get_threads() -> usize {
     available_parallelism().unwrap().get()
+}
+
+#[inline(never)]
+fn immix_noop1(_u: usize) {}
+
+macro_rules! keep_on_stack {
+    ($($e: expr),*) => {
+        $(
+            immix_noop1($e as *const _ as usize)
+        )*
+    }
+}
+
+static mut ROOT: *mut u8 = 0 as *mut _;
+
+unsafe fn bdw_alloc_test_obj(space: &mut Heap) -> *mut GCTestObj {
+    let a = space.alloc_raw::<GCTestObj>();
+    // let a = malloc(size_of::<GCTestObj>()) as *mut GCTestObj;
+    a.write(GCTestObj {
+        _vtable: gctest_vtable,
+        b: std::ptr::null_mut(),
+        d: std::ptr::null_mut(),
+        e: std::ptr::null_mut(),
+    });
+    // GC_register_finalizer(a as * mut u8, fin, null_mut(), null_mut(), null_mut());
+    a
+}
+fn bdwgcbench(space: &mut Heap) -> Duration {
+    unsafe {
+        let t = std::time::Instant::now();
+        let mut long_lived = bdw_alloc_test_obj(space);
+        let rustptr = (&mut long_lived) as *mut *mut GCTestObj as *mut u8;
+        ROOT = rustptr;
+        space.add_root(rustptr);
+        bdw_populate(K_LONG_LIVED_TREE_DEPTH, long_lived, space);
+        // let _tt = space.collect();
+        // println!("ep: {:?}", ep);
+        let mut d = K_MIN_TREE_DEPTH;
+        while d <= K_MAX_TREE_DEPTH {
+            bdw_time_construction(d, space);
+            // Populate(d,  long_lived, space);
+            d += 2;
+            // println!("ep: {:?}", ep);
+        }
+        // let _tt = space.collect();
+        // space.remove_root(rustptr);
+        // let _tt = space.collect();
+        // println!("ep: {:?}", ep);
+        // sleep(Duration::from_secs(100000));
+        space.remove_root(rustptr);
+        keep_on_stack!(&mut long_lived);
+        let t = t.elapsed();
+        // println!("time: {:?}", t);
+        // tt.0 + tt.1
+        t
+    }
+}
+
+#[inline(never)]
+unsafe fn bdw_populate(idepth: i32, thisnode: *mut GCTestObj, space: &mut Heap) {
+    if idepth <= 0 {
+        return;
+    }
+    (*thisnode).b = bdw_alloc_test_obj(space);
+    (*thisnode).e = bdw_alloc_test_obj(space);
+    bdw_populate(idepth - 1, (*thisnode).e, space);
+    bdw_populate(idepth - 1, (*thisnode).b, space);
+}
+#[inline(never)]
+unsafe fn bdw_make_tree(idepth: i32, space: &mut Heap) -> *mut GCTestObj {
+    if idepth <= 0 {
+        return bdw_alloc_test_obj(space);
+    } else {
+        let mut left = bdw_make_tree(idepth - 1, space);
+        let rustptr1 = (&mut left) as *mut *mut GCTestObj as *mut u8;
+        space.add_root(rustptr1);
+        let mut right = bdw_make_tree(idepth - 1, space);
+        let rustptr2 = (&mut right) as *mut *mut GCTestObj as *mut u8;
+        space.add_root(rustptr2);
+        let mut result = bdw_alloc_test_obj(space);
+        let rustptr3 = (&mut result) as *mut *mut GCTestObj as *mut u8;
+        space.add_root(rustptr3);
+        (*result).b = left;
+        (*result).e = right;
+        space.remove_root(rustptr2);
+        space.remove_root(rustptr1);
+        space.remove_root(rustptr3);
+        result
+    }
+}
+#[inline(never)]
+unsafe fn bdw_time_construction(depth: i32, space: &mut Heap) {
+    let i_num_iters = num_iters(depth);
+
+    for _ in 0..i_num_iters {
+        let mut temp_tree = bdw_alloc_test_obj(space);
+        let rustptr = (&mut temp_tree) as *mut *mut GCTestObj as *mut u8;
+        space.add_root(rustptr);
+        bdw_populate(depth, temp_tree, space);
+        space.remove_root(rustptr);
+        keep_on_stack!(temp_tree)
+        // destroy tempTree
+    }
+
+    for _ in 0..i_num_iters {
+        let _temp_tree = bdw_make_tree(depth, space);
+        keep_on_stack!(_temp_tree)
+    }
 }
 
 criterion_group!(benches, immix_benchmark,);
